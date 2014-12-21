c2666cb30939e5db0f6ee1551359eb863bbba859
Justin Lubin <jlubi333@gmail.com>
2014-12-21 11:55:39 -0500

    Improve Jade

--- jade.hs
+++ jade.hs
@@ -4,11 +4,13 @@ data Function = Constant Double
               | X
               | Sum Function Function
               | Product Function Function
+              | Quotient Function Function
               | Polynomial Power Function
               | Sin Function
               | Cos Function
               | Exponential Base Function
               | Log Base Function
+              | Undefined
               deriving (Eq)
 
 instance Show Function where
@@ -16,23 +18,30 @@ instance Show Function where
     show X = "x"
     show (Sum u v) = (show u) ++ " + " ++ (show v)
     show (Product (Constant (-1)) f) = "-" ++ (show f)
+    show (Product f (Constant (-1))) = "-" ++ (show f)
     show (Product (Constant a) f) = (intShow a) ++ (show f)
     show (Product u v) = "(" ++ (show u) ++ ") * (" ++ (show v) ++ ")"
-    show (Polynomial p f) = "(" ++ (show f) ++ ")^" ++ (intShow p)
+    show (Quotient u v) = "(" ++ (show u) ++ ") / (" ++ (show v) ++ ")"
+    show (Polynomial p f)
+        | f == X    = (show f) ++ "^" ++ (intShow p)
+        | otherwise = "(" ++ (show f) ++ ")^" ++ (intShow p)
     show (Sin f) = "sin(" ++ (show f) ++ ")"
     show (Cos f) = "cos(" ++ (show f) ++ ")"
     show (Exponential b f) = (intShow b) ++ "^" ++ "(" ++ (show f) ++ ")"
     show (Log b f) = "log" ++ (intShow b) ++ "(" ++ (show f) ++ ")"
+    show Undefined = "Undefined"
 
 derivative :: Function -> Function
 derivative (Constant _) = Constant 0
 derivative X = Constant 1
 derivative (Sum u v) = Sum (derivative u) (derivative v)
 derivative (Product u v) = Sum (Product (derivative u) v) (Product u (derivative v)) 
+derivative (Quotient u v) = (Quotient (Sum (Product v (derivative u)) (Product (Product (Constant (-1)) u) (derivative v))) (Polynomial 2 v))
 derivative (Polynomial p f) = Product (derivative f) (Product (Constant p) (Polynomial (p - 1) f))
-derivative (Sin f) = Product (derivative f) (Product (Constant (-1)) (Cos f))
-derivative (Cos f) = Product (derivative f) (Sin f)
+derivative (Sin f) = Product (derivative f) (Cos f)
+derivative (Cos f) = Product (derivative f) (Product (Constant (-1)) (Sin f))
 derivative (Exponential b f) = Product (derivative f) (Product (Exponential b f) (Log 2.71 (Constant b)))
+derivative Undefined = Undefined
 
 simplify :: Function -> Function
 simplify (Sum f (Constant 0)) = f
@@ -46,12 +55,20 @@ simplify (Sum u v)
 simplify (Product f (Constant 0)) = Constant 0
 simplify (Product (Constant 0) f) = Constant 0
 simplify (Product f (Constant 1)) = f
-simplify (Product (Constant 1.0) f) = f
+simplify (Product (Constant 1) f) = f
 simplify (Product (Constant a) (Constant b)) = Constant (a * b)
 simplify (Product u v)
     | u == v    = Polynomial 2 u
     | otherwise = Product (simplify u) (simplify v)
+simplify (Quotient f (Constant 0)) = Undefined
+simplify (Quotient (Constant 0) f) = Constant 0
+simplify (Quotient f (Constant 1)) = f
+simplify (Quotient u v)
+    | u == v    = Constant 1
+    | otherwise = Quotient (simplify u) (simplify v)
 simplify (Polynomial 1 f) = f
+simplify (Polynomial 0 (Constant 0)) = Undefined
+simplify (Polynomial _ (Constant 0)) = Constant 0
 simplify f = f
 
 simplifyIteration :: Function -> [Function]
@@ -59,6 +76,9 @@ simplifyIteration = iterate simplify
 
 fullySimplify :: Function -> Function
 fullySimplify f = simplifyIteration f !! 100 
+
+d = fullySimplify . derivative
+fs = fullySimplify
 
 evaluate :: Function -> Double -> Double
 evaluate (Constant a) x = a
@@ -70,8 +90,7 @@ evaluate (Sin f) x = sin (evaluate f x)
 evaluate (Cos f) x = cos (evaluate f x)
 evaluate (Exponential b f) x = b ** (evaluate f x)
 evaluate (Log b f) x = logBase b (evaluate f x)
-
-d = fullySimplify . derivative
+evaluate Undefined x = undefined
 
 isInt :: Double -> Double -> Bool
 isInt precision x = x - fromInteger (round x) < precision
